a613a4c83e4cfd624eacbc90705b310a7f0e994c
royalcat <k.adamovich20@gmail.com>
2024-07-16 17:33:14 +0300

    [fix] reduce distance treshold,  fix run main

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 	"runtime"
+	"strconv"
 	"strings"
+	"time"
 
+	"github.com/fasthttp/router"
+	"github.com/paulmach/orb"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/royalcat/rgeocache/geocoder"
+	"github.com/royalcat/rgeocache/geomodel"
 	"github.com/royalcat/rgeocache/geoparser"
+	"github.com/valyala/fasthttp"
+	"github.com/valyala/fasthttp/fasthttpadaptor"
 
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v3"
@@ -15,16 +28,6 @@ import (
 )
 
 func main() {
-	// f, err := os.Create("cpu.prof")
-	// if err != nil {
-	// 	log.Fatal("could not create CPU profile: ", err)
-	// }
-	// defer f.Close() // error handling omitted for example
-	// if err := pprof.StartCPUProfile(f); err != nil {
-	// 	log.Fatal("could not start CPU profile: ", err)
-	// }
-	// defer pprof.StopCPUProfile()
-
 	app := &cli.App{
 		Name:        "rgeocache",
 		Description: "Reverse geocoder with pregenerated cache",
@@ -133,14 +136,129 @@ func generate(ctx *cli.Context) error {
 }
 
 func serve(ctx *cli.Context) error {
-	srv := &Server{
-		rgeo: &geocoder.RGeoCoder{},
-	}
 	log := logrus.New()
 	log.Info("Initing geocoder")
-	err := srv.rgeo.LoadFromPointsFile(ctx.String("points"))
+	rgeo := &geocoder.RGeoCoder{}
+	err := rgeo.LoadFromPointsFile(ctx.String("points"))
 	if err != nil {
 		return err
 	}
-	return srv.ListenAndServe(ctx.Context, ctx.String("listen"))
+
+	return runServer(ctx.Context, ctx.String("listen"), rgeo)
 }
+
+func runServer(ctx context.Context, address string, rgeo *geocoder.RGeoCoder) error {
+	log := logrus.New()
+
+	s := &server{
+		rgeo: rgeo,
+	}
+
+	r := router.New()
+	r.GET("/rgeocode/address/{lat}/{lon}", s.RGeoCodeHandler)
+	r.GET("/rgeocode/multiaddress", s.RGeoMultipleCodeHandler) // DEPRECATED use post endpoint
+	r.POST("/rgeocode/multiaddress", s.RGeoMultipleCodeHandler)
+	r.Handle(http.MethodGet, "/metrics", fasthttpadaptor.NewFastHTTPHandler(promhttp.Handler()))
+
+	server := &fasthttp.Server{
+		ReadTimeout: time.Second,
+		Handler:     r.Handler,
+	}
+	go func() {
+		log.Infof("Server listening on: %s", address)
+		if err := server.ListenAndServe(address); err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe(): %v", err)
+		}
+	}()
+	logrus.Info("Server started")
+
+	// wait cancel
+	<-ctx.Done()
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	return server.ShutdownWithContext(shutdownCtx)
+}
+
+type server struct {
+	rgeo *geocoder.RGeoCoder
+}
+
+func (s *server) RGeoCodeHandler(ctx *fasthttp.RequestCtx) {
+	metricHttpAdressCallCount.Inc()
+
+	latS := ctx.UserValue("lat").(string)
+	lonS := ctx.UserValue("lon").(string)
+
+	lat, err := strconv.ParseFloat(latS, 64)
+	if err != nil {
+		ctx.Response.SetStatusCode(http.StatusBadRequest)
+		return
+	}
+	lon, err := strconv.ParseFloat(lonS, 64)
+	if err != nil {
+		ctx.Response.SetStatusCode(http.StatusBadRequest)
+		return
+	}
+
+	i, ok := s.rgeo.Find(lat, lon)
+	if !ok {
+		ctx.Response.SetStatusCode(http.StatusNoContent)
+		return
+	}
+
+	body, err := json.Marshal(i)
+	if err != nil {
+		ctx.Response.SetStatusCode(http.StatusInternalServerError)
+		return
+	}
+
+	ctx.Response.SetStatusCode(http.StatusOK)
+	ctx.Response.BodyWriter().Write(body)
+	return
+}
+
+func (s *server) RGeoMultipleCodeHandler(ctx *fasthttp.RequestCtx) {
+	metricHttpMultiAdressCallCount.Inc()
+
+	req := []orb.Point{} // latitude, longitude
+	err := json.Unmarshal(ctx.Request.Body(), &req)
+	if err != nil {
+		ctx.Response.SetStatusCode(http.StatusBadRequest)
+		return
+	}
+
+	res := []geomodel.Info{}
+	for _, p := range req {
+		info, _ := s.rgeo.Find(p[0], p[1])
+		res = append(res, info.Info)
+	}
+
+	body, err := json.Marshal(res)
+	if err != nil {
+		ctx.Response.SetStatusCode(http.StatusInternalServerError)
+		return
+	}
+
+	ctx.Response.SetStatusCode(http.StatusOK)
+	ctx.Response.BodyWriter().Write(body)
+	return
+}
+
+var (
+	metricHttpAdressCallCount = promauto.NewCounter(
+		prometheus.CounterOpts{
+			Namespace: "rgeocode",
+			Subsystem: "http_address",
+			Name:      "call_count_total",
+			Help:      "count of address interactions",
+		},
+	)
+	metricHttpMultiAdressCallCount = promauto.NewCounter(
+		prometheus.CounterOpts{
+			Namespace: "rgeocode",
+			Subsystem: "http_multi_address",
+			Name:      "call_count_total",
+			Help:      "count of address interactions",
+		},
+	)
+)
