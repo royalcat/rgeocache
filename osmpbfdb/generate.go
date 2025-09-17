b0a93bb1f0fa70dde4e8916dcbe5280c5e98bb9a
royalcat <k.adamovich20@gmail.com>
2025-09-17 16:08:43 +0400

    shared osmpbfdb

--- osmpbfdb/generate.go
+++
@@ -1,3 +0,0 @@
-package osmpbfdb
-
-//go:generate protoc  --proto_path=. --go_opt=module=github.com/royalcat/rgeocache/kv/osmpbfdb/osmproto  --go_out=osmproto osmproto/fileformat.proto osmproto/osmformat.proto
