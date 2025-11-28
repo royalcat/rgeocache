bda3d1fbc4229ac275212ab77f5c5199d8edf50e
royalcat <k.adamovich20@gmail.com>
2025-11-28 21:54:02 +0400

    FindInRadius method

--- geocoder/locator.go
+++ geocoder/locator.go
@@ -23,9 +23,13 @@ type InfoModel struct {
 }
 
 func (f *RGeoCoder) Find(lat, lon float64) (i InfoModel, ok bool) {
+	return f.FindInRadius(lat, lon, f.searchRadius)
+}
+
+func (f *RGeoCoder) FindInRadius(lat, lon float64, radius float64) (i InfoModel, ok bool) {
 	finPoint := kdbush.Point[geomodel.Info]{}
 	finDist := math.Inf(1)
-	f.tree.Within(lon, lat, f.searchRadius, func(p kdbush.Point[geomodel.Info]) bool {
+	f.tree.Within(lon, lat, radius, func(p kdbush.Point[geomodel.Info]) bool {
 		dist := distanceSquared(lon, lat, p.X, p.Y)
 		if dist < finDist {
 			finPoint = p
