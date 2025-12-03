4d2a93dc0eb2937f1cacb6e602579375ee059234
royalcat <k.adamovich20@gmail.com>
2025-12-03 13:26:33 +0400

    huge tree seach copy optimization

--- geocoder/loading.go
+++ geocoder/loading.go
@@ -24,7 +24,7 @@ func loadOptions(opts ...Option) options {
 	return options
 }
 
-func newRGeoCoder(tree *kdbush.KDBush[geomodel.Info], opts ...Option) *RGeoCoder {
+func newRGeoCoder(tree *kdbush.KDBush[*geomodel.Info], opts ...Option) *RGeoCoder {
 	options := loadOptions(opts...)
 	options.logger.Info("Initializing geocoder")
 
@@ -36,7 +36,7 @@ func newRGeoCoder(tree *kdbush.KDBush[geomodel.Info], opts ...Option) *RGeoCoder
 }
 
 func NewRGeoCoder(opts ...Option) *RGeoCoder {
-	return newRGeoCoder(kdbush.NewBush[geomodel.Info](nil, 256), opts...)
+	return newRGeoCoder(kdbush.NewBush[*geomodel.Info](nil, 256), opts...)
 }
 
 func LoadGeoCoderFromReader(r io.Reader, opts ...Option) (*RGeoCoder, error) {
@@ -44,13 +44,22 @@ func LoadGeoCoderFromReader(r io.Reader, opts ...Option) (*RGeoCoder, error) {
 	log := options.logger
 
 	log.Info("Loading geocoder points from reader")
-	points, err := cachesaver.LoadFromReader(r, log)
+	pointsRaw, err := cachesaver.LoadFromReader(r, log)
 	if err != nil {
 		return nil, fmt.Errorf("error loading points: %s", err.Error())
 	}
+	points := convertPointsDataToPointer(pointsRaw)
 
 	tree := kdbush.NewBush(points, 256)
 	return newRGeoCoder(tree, opts...), nil
+}
+
+func convertPointsDataToPointer[T any](points []kdbush.Point[T]) []kdbush.Point[*T] {
+	result := make([]kdbush.Point[*T], len(points))
+	for i, point := range points {
+		result[i] = kdbush.Point[*T]{X: point.X, Y: point.Y, Data: &point.Data}
+	}
+	return result
 }
 
 func LoadGeoCoderFromFile(file string, opts ...Option) (*RGeoCoder, error) {
@@ -69,10 +78,11 @@ func (f *RGeoCoder) LoadFromPointsFile(file string) error {
 		return fmt.Errorf("error opening points file: %s", err.Error())
 	}
 
-	points, err := cachesaver.LoadFromReader(reader, slog.Default())
+	pointsRaw, err := cachesaver.LoadFromReader(reader, slog.Default())
 	if err != nil {
 		return fmt.Errorf("error loading points: %s", err.Error())
 	}
+	points := convertPointsDataToPointer(pointsRaw)
 
 	f.tree = kdbush.NewBush(points, 256)
 	return nil
