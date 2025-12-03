90b19eac95c50d461d951c51f63eb6a7193fe290
royalcat <k.adamovich20@gmail.com>
2025-12-03 13:26:33 +0400

    huge tree seach copy optimization

--- geocoder/locator.go
+++ geocoder/locator.go
@@ -9,7 +9,7 @@ import (
 )
 
 type RGeoCoder struct {
-	tree *kdbush.KDBush[geomodel.Info]
+	tree *kdbush.KDBush[*geomodel.Info]
 
 	searchRadius float64
 	logger       *slog.Logger
@@ -27,9 +27,9 @@ func (f *RGeoCoder) Find(lat, lon float64) (i InfoModel, ok bool) {
 }
 
 func (f *RGeoCoder) FindInRadius(lat, lon float64, radius float64) (i InfoModel, ok bool) {
-	finPoint := kdbush.Point[geomodel.Info]{}
+	finPoint := kdbush.Point[*geomodel.Info]{}
 	finDist := math.Inf(1)
-	f.tree.Within(lon, lat, radius, func(p kdbush.Point[geomodel.Info]) bool {
+	f.tree.Within(lon, lat, radius, func(p kdbush.Point[*geomodel.Info]) bool {
 		dist := distanceSquared(lon, lat, p.X, p.Y)
 		if dist < finDist {
 			finPoint = p
@@ -47,7 +47,7 @@ func (f *RGeoCoder) FindInRadius(lat, lon float64, radius float64) (i InfoModel,
 		return InfoModel{}, false
 	}
 
-	return InfoModel{Info: finPoint.Data}, true
+	return InfoModel{Info: *finPoint.Data}, true
 }
 
 func distanceSquared(x1, y1, x2, y2 float64) (distance float64) {
