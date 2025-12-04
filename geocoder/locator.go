eab3b6c444fcf41c1cf86965cc215b661d9010d5
royalcat <k.adamovich20@gmail.com>
2025-12-04 19:09:36 +0400

    memory optimization

--- geocoder/locator.go
+++ geocoder/locator.go
@@ -3,20 +3,37 @@ package geocoder
 import (
 	"log/slog"
 	"math"
+	"unique"
 
 	"github.com/royalcat/rgeocache/geomodel"
 	"github.com/royalcat/rgeocache/kdbush"
 )
 
+// internal memory-optimized representation of geomodel.Info
+type geoInfo struct {
+	Name        string
+	Street      unique.Handle[string]
+	HouseNumber unique.Handle[string]
+	City        unique.Handle[string]
+	Region      unique.Handle[string]
+}
+
+func (g *geoInfo) value() geomodel.Info {
+	return geomodel.Info{
+		Name:        g.Name,
+		Street:      g.Street.Value(),
+		HouseNumber: g.HouseNumber.Value(),
+		City:        g.City.Value(),
+		Region:      g.Region.Value(),
+	}
+}
+
 type RGeoCoder struct {
-	tree *kdbush.KDBush[*geomodel.Info]
+	tree *kdbush.KDBush[*geoInfo]
 
 	searchRadius float64
 	logger       *slog.Logger
 }
-
-const maxSearchRadius float64 = 0.01
-const thresholdRadius float64 = 1e-7
 
 type InfoModel struct {
 	geomodel.Info
@@ -27,9 +44,9 @@ func (f *RGeoCoder) Find(lat, lon float64) (i InfoModel, ok bool) {
 }
 
 func (f *RGeoCoder) FindInRadius(lat, lon float64, radius float64) (i InfoModel, ok bool) {
-	finPoint := kdbush.Point[*geomodel.Info]{}
+	finPoint := kdbush.Point[*geoInfo]{}
 	finDist := math.Inf(1)
-	f.tree.Within(lon, lat, radius, func(p kdbush.Point[*geomodel.Info]) bool {
+	f.tree.Within(lon, lat, radius, func(p kdbush.Point[*geoInfo]) bool {
 		dist := distanceSquared(lon, lat, p.X, p.Y)
 		if dist < finDist {
 			finPoint = p
@@ -47,7 +64,7 @@ func (f *RGeoCoder) FindInRadius(lat, lon float64, radius float64) (i InfoModel,
 		return InfoModel{}, false
 	}
 
-	return InfoModel{Info: *finPoint.Data}, true
+	return InfoModel{Info: finPoint.Data.value()}, true
 }
 
 func distanceSquared(x1, y1, x2, y2 float64) (distance float64) {
