df3123d33e0a080b9a352d934d0c8acc90a36f57
royalcat <k.adamovich20@gmail.com>
2025-12-11 06:08:20 +0400

    skip parsed osm objects

---
+++ geoparser/set.go
@@ -0,0 +1,43 @@
+package geoparser
+
+import "sync"
+
+type set[K comparable] struct {
+	mu    sync.RWMutex
+	items map[K]struct{}
+}
+
+func newSet[K comparable]() *set[K] {
+	return &set[K]{
+		items: make(map[K]struct{}),
+	}
+}
+
+func (s *set[K]) Add(item K) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.items[item] = struct{}{}
+}
+
+func (s *set[K]) Contains(item K) bool {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	_, ok := s.items[item]
+	return ok
+}
+
+func (s *set[K]) Remove(item K) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.items, item)
+}
+
+func (s *set[K]) ContainsAndAdd(item K) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if _, ok := s.items[item]; !ok {
+		s.items[item] = struct{}{}
+		return true
+	}
+	return false
+}
