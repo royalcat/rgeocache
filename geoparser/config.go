d8ca6cc6684c431024f6597566995f74681bcbcb
royalcat <k.adamovich20@gmail.com>
2026-01-26 19:06:00 +0400

    reduce amount of points for highways and areas

--- geoparser/config.go
+++ geoparser/config.go
@@ -14,6 +14,6 @@ func ConfigDefault() Config {
 		Threads:               runtime.GOMAXPROCS(-1),
 		Version:               1,
 		PreferredLocalization: "",
-		HighwayPointsDistance: 100,
+		HighwayPointsDistance: 150,
 	}
 }
