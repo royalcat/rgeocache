be7296c3cfb37c7f9b7b16ed1582d7a248eace0b
royalcat <k.adamovich20@gmail.com>
2026-07-02 00:37:39 +0400

    parallel save

--- geoparser/save.go
+++ geoparser/save.go
@@ -2,15 +2,19 @@ package geoparser
 
 import (
 	"cmp"
+	"fmt"
+	"iter"
 	"slices"
+	"sync"
 	"time"
 	"unique"
 
 	"github.com/royalcat/rgeocache/cachesaver"
 	cachemodel "github.com/royalcat/rgeocache/cachesaver/model"
+	"golang.org/x/sync/errgroup"
 )
 
-func (f *GeoGen) saveWorker() error {
+func (f *GeoGen) saveWorker(outputs []ParseOutput) error {
 	points := func(yield func(cachemodel.Point) bool) {
 		for point := range f.parsedPoints {
 			if !yield(cachesaver.Point{
@@ -62,10 +66,26 @@ func (f *GeoGen) saveWorker() error {
 		DateCreated: time.Now(),
 	}
 
-	if f.config.CacheFormat == "2" {
-		return cachesaver.SaveV2(points, zones, meta, f.output)
+	pointsTee := Tee(points, len(outputs), 1)
+	zonesTee := Tee(zones, len(outputs), 1)
+
+	var wg errgroup.Group
+	for i, output := range outputs {
+		switch output.Format {
+		case "v1":
+			wg.Go(func() error {
+				return cachesaver.SaveV1(pointsTee[i], zonesTee[i], meta, output.Writer)
+			})
+		case "v2":
+			wg.Go(func() error {
+				return cachesaver.SaveV2(pointsTee[i], zonesTee[i], meta, output.Writer)
+			})
+		default:
+			return fmt.Errorf("unsupported format: %s", output.Format)
+		}
 	}
-	return cachesaver.SaveV1(points, zones, meta, f.output)
+
+	return wg.Wait()
 }
 
 func uniqueGeoPoints(points []geoPoint) []geoPoint {
@@ -84,4 +104,55 @@ func uniqueGeoPoints(points []geoPoint) []geoPoint {
 		return cmp.Compare(cantorPairFunc(a.X(), a.Y()), cantorPairFunc(b.X(), b.Y()))
 	})
 	return slices.Compact(points)
+}
+
+// Tee duplicates a source iterator into 'n' independent copies.
+func Tee[V any](src iter.Seq[V], n int, bufferSize int) []iter.Seq[V] {
+	// A slice of channels to hold data for each copy
+	chs := make([]chan V, n)
+	for i := range chs {
+		chs[i] = make(chan V, bufferSize)
+	}
+
+	// Fan out data from the source iterator
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer func() {
+			for _, ch := range chs {
+				close(ch) // Close all channels when src is exhausted
+			}
+		}()
+
+		for v := range src {
+			for _, ch := range chs {
+				ch <- v
+			}
+		}
+	}()
+
+	// Create 'n' independent iterators from the channels
+	iters := make([]iter.Seq[V], n)
+	for i := range chs {
+		ch := chs[i]
+		iters[i] = func(yield func(V) bool) {
+			for {
+				v, ok := <-ch
+				if !ok {
+					break // Channel closed, this specific iterator exits
+				}
+				if !yield(v) {
+					// Loop body exited early (break), disconnect this channel
+					// by draining it to prevent the source goroutine from blocking.
+					go func() {
+						for range ch {
+						}
+					}()
+					break
+				}
+			}
+		}
+	}
+
+	return iters
 }
