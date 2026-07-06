918a3fe9d26de3f436650fe4a0abf48c524b5e8e
royalcat <k.adamovich20@gmail.com>
2026-07-06 23:57:18 +0400

    docker build

--- server_rs/build.rs
+++ server_rs/build.rs
@@ -3,10 +3,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // CacheMetadata from v1 is parsed manually (3 simple fields).
     prost_build::Config::new()
         .compile_protos(
-            &["../cachesaver/save/v2/proto/cache_v2.proto"],
-            &["../cachesaver/save/v2/proto"],
+            &["proto/cache_v2.proto"],
+            &["proto"],
         )?;
 
-    println!("cargo:rerun-if-changed=../cachesaver/save/v2/proto/cache_v2.proto");
+    println!("cargo:rerun-if-changed=proto/cache_v2.proto");
     Ok(())
 }
